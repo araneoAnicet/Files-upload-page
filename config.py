3c05929ad39542fd0fe747621830a3963704d671
araneoAnicet <root@localhost.localdomain>
2019-04-02 13:02:08 +0100

    clear config, add constants

--- config.py
+++ config.py
@@ -1,25 +1,3 @@
-import os
-from datetime import datetime
-from flask import request
-from datetime import datetime
-
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif'])
+UPLOAD_FOLDER = 'stored_images'
 SECRET = 'H@xwaid2q8YH@IYE*(hfWI#NO:#@(S!OSjfa'
-IMG_FOLDER = 'static/stored_images/'
-
-def file_upload(upload_path):
-    target = os.path.join(os.path.dirname(os.path.abspath(__file__)), upload_path)
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    for file in request.files.getlist('file'):
-        print('\nLOADING .  .   .   .  {}'.format(file.filename))
-        print('    UPLOAD DATE:     {}'.format(str(datetime.now())))
-        modified_name = datetime.today().strftime('%Y-%m-%d-%H:%M:%s') + '___' + file.filename
-        destination = '/'.join([target, modified_name])
-        file.save(destination)
-    print('.    .   .   .FILES UPLOADED  .   .   .   .\n')
-
-def folder_content(link='static/stored_images/'):
-    if not os.path.exists(link):
-        return None
-    for img_name in os.listdir(link):
-        yield link + img_name
--- root_control.py
+++ root_control.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, flash
-from flask_bootstrap import Bootstrap
-from config import IMG_FOLDER, SECRET, file_upload, folder_content
+from objects_definitions import app
+from flask import render_template, redirect, url_for, flash
+from config import UPLOAD_FOLDER
+from file_management import db, Image, file_upload, folder_content, remove_deleted_files
+from api_management import add_resources
 
-app = Flask(__name__)
-app.secret_key = SECRET
-Bootstrap(app)
+
+add_resources()  # adds API resources, definitions are in api_management.py
 
 @app.route('/')
 def index():
@@ -13,14 +14,20 @@ def index():
 
 @app.route('/load', methods=['POST'])
 def load():
-    file_upload(IMG_FOLDER)
+    file_upload(db, Image, UPLOAD_FOLDER)
     flash('FILES WERE UPLOADED SUCCESSFULLY!')
     return redirect(url_for('index'))
 
 
-@app.route('/images', methods=['GET'])
-def images():
-    return render_template('images.html', files=folder_content())
+@app.route('/images')
+def images(api_option=None):
+    return render_template('images.html', files=folder_content('static/' + UPLOAD_FOLDER + '/'))
+
+@app.route('/api')
+def api():
+    return render_template('api_documentation.html')
+    
 
 if __name__ == '__main__':
+    remove_deleted_files(db, Image, UPLOAD_FOLDER)  # optional: removes data of deleted files from database
     app.run(debug=True)
