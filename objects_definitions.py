e6c59271464e0f958e304607190c1ad2ab92f7a2
araneoAnicet <root@localhost.localdomain>
2019-04-02 13:04:45 +0100

    create a module for storing globally used objects

---
+++ objects_definitions.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import SECRET
+from flask_cors import CORS
+from flask_bootstrap import Bootstrap
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+CORS(app)
+api = Api(app)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.secret_key = SECRET
+Bootstrap(app)
