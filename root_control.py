0154ee34c0451957cf8427c255d95ba4bab6f8b4
araneoAnicet <root@localhost.localdomain>
2019-04-03 17:07:04 +0100

    remove api management module

--- root_control.py
+++ root_control.py
@@ -1,11 +1,12 @@
-from objects_definitions import app
-from flask import render_template, redirect, url_for, flash
-from config import UPLOAD_FOLDER
-from file_management import db, Image, file_upload, folder_content, remove_deleted_files
-from api_management import add_resources
+from objects_definitions import app, json_response
+from flask import render_template, redirect, url_for, flash, request, jsonify
+from config import UPLOAD_FOLDER, API_UPLOAD_FOLDER
+from file_management import file_upload, folder_content
+import os
+#from api_management import add_resources
 
 
-add_resources()  # adds API resources, definitions are in api_management.py
+#add_resources()  # adds API resources, definitions are in api_management.py
 
 @app.route('/')
 def index():
@@ -20,14 +21,60 @@ def load():
 
 
 @app.route('/images')
-def images(api_option=None):
+def images():
     return render_template('images.html', files=folder_content('static/' + UPLOAD_FOLDER + '/'))
+
 
 @app.route('/api')
 def api():
     return render_template('api_documentation.html')
+
+
+# REST
+@app.route('/api/images/<hash>', methods=['GET'])
+def api_image(hash):
+    if folder_files('static/' + API_UPLOAD_FOLDER + '/' + hash):
+        return jsonify({
+            'massage': 'Returned images',
+            'status': 200,
+            'data': {'hash': hash},
+            'request_payload': {'url': '/api/images/' + hash}
+        })
+    return jsonify({
+        'message': 'Image was not found',
+        'status': 404,
+        'data': {'hash': hash},
+        'request_payload': {
+            'url': '/api/images/' + hash,
+            'method': 'GET',
+            'headers': None
+            }
+    })
     
+@app.route('/api/images/<hash>', methods=['DELETE'])
+def api_image(hash):
+    if folder_files('static/uploads' + hash):
+        os.remove('static/' + API_UPLOAD_FOLDER + '/' + hash)
+        return jsonify({
+            'message': 'Image was deleted successfully',
+            'status': 200,
+            'data': {'hash': hash},
+            'request_payload': {
+                'url': '/api/images/' + hash,
+                'method': 'DELETE',
+                'headers': None
+                }
+        })
+    return jsonify({
+        'message': 'Image was not found',
+        'status': 404,
+        'data': {'hash': hash},
+        'request_payload': {
+            'url': '/api/images' + hash,
+            'method': 'DELETE',
+            'headers': None
+        }
+    })
 
 if __name__ == '__main__':
-    remove_deleted_files(db, Image, UPLOAD_FOLDER)  # optional: removes data of deleted files from database
     app.run(debug=True)
