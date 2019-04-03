4b9e8544d39118575e0ffa39fe09fff256195cc0
araneoAnicet <root@localhost.localdomain>
2019-04-03 17:51:24 +0100

    route api

--- root_control.py
+++ root_control.py
@@ -1,12 +1,9 @@
-from objects_definitions import app, json_response
+from objects_definitions import app
 from flask import render_template, redirect, url_for, flash, request, jsonify
 from config import UPLOAD_FOLDER, API_UPLOAD_FOLDER
-from file_management import file_upload, folder_content
+from file_management import file_upload, folder_content, folder_files, folders_check
 import os
-#from api_management import add_resources
 
-
-#add_resources()  # adds API resources, definitions are in api_management.py
 
 @app.route('/')
 def index():
@@ -31,19 +28,74 @@ def api():
 
 
 # REST
+@app.route('/api/images', methods=['POST'])
+def api_post_image():
+    saved_files = [filename for filename in file_upload(API_UPLOAD_FOLDER)]
+    if saved_files:
+        return jsonify({
+            'message': '{} images were uploaded successfully'.format(len(saved_files)),
+            'status': 200,
+            'data': list(map(lambda img_hash: {'hash': img_hash} , saved_files)),
+            'request_payload': {
+                'url': '/api/images',
+                'method': 'POST',
+                'headers': None
+            }
+        })
+    return jsonify({
+        'message': 'No images were selected',
+        'status': 404,
+        'data': None,
+        'request_payload': {
+            'url': '/api/images',
+            'method': 'POST',
+            'headers': None
+        }
+    })
+
+@app.route('/api/images', methods=['GET'])
+def api_get_grid():
+    grid_files = [filename for filename in folder_files('static/' + API_UPLOAD_FOLDER + '/')]
+    if grid_files:
+        return jsonify({
+            'message': 'Returned {} images'.format(len(grid_files)),
+            'status': 200,
+            'data': list(map(lambda img_hash: {'hash': img_hash} , grid_files)),
+            'request_payload': {
+                'url': '/api/images',
+                'method': 'GET',
+                'headers': None
+                }
+        })
+    return jsonify({
+            'message': 'No images found',
+            'status': 404,
+            'data': None,
+            'request_payload': {
+                'url': '/api/images',
+                'method': 'GET',
+                'headers': None
+                }
+        })
+
+
 @app.route('/api/images/<hash>', methods=['GET'])
-def api_image(hash):
+def api_get_img(hash):
     if folder_files('static/' + API_UPLOAD_FOLDER + '/' + hash):
         return jsonify({
-            'massage': 'Returned images',
+            'massage': 'Returned image',
             'status': 200,
             'data': {'hash': hash},
-            'request_payload': {'url': '/api/images/' + hash}
+            'request_payload': {
+                'url': '/api/images/' + hash,
+                'method': 'GET',
+                'headers': None
+                }
         })
     return jsonify({
         'message': 'Image was not found',
         'status': 404,
-        'data': {'hash': hash},
+        'data': None,
         'request_payload': {
             'url': '/api/images/' + hash,
             'method': 'GET',
@@ -52,7 +104,7 @@ def api_image(hash):
     })
     
 @app.route('/api/images/<hash>', methods=['DELETE'])
-def api_image(hash):
+def api_delete_img(hash):
     if folder_files('static/uploads' + hash):
         os.remove('static/' + API_UPLOAD_FOLDER + '/' + hash)
         return jsonify({
@@ -68,7 +120,7 @@ def api_image(hash):
     return jsonify({
         'message': 'Image was not found',
         'status': 404,
-        'data': {'hash': hash},
+        'data': None,
         'request_payload': {
             'url': '/api/images' + hash,
             'method': 'DELETE',
@@ -77,4 +129,5 @@ def api_image(hash):
     })
 
 if __name__ == '__main__':
+    folders_check([UPLOAD_FOLDER, API_UPLOAD_FOLDER])
     app.run(debug=True)
