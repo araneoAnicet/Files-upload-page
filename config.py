8497c93211d73813ee9fcf0cb231d6004e84898a
araneoAnicet <root@localhost.localdomain>
2019-04-03 16:09:04 +0100

    add admin token

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif'])
 UPLOAD_FOLDER = 'stored_images'
 SECRET = 'H@xwaid2q8YH@IYE*(hfWI#NO:#@(S!OSjfa'
+ADMIN_TOKEN = ''
+json_structure = {message:'', statusCode: 200, data:'', requestPayload:{}}
